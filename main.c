b971175048101b838a5d26e00e8c74b8e76469dc
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-09-20 21:24:41 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	return 0;
+}
+int is_pivot(float arr [20], int arr_size, int pivot_index)
+{
+	int i = 0, cnt1 = 0,cnt2 = 0;
+	while(i<pivot_index){
+		if(arr[i]<=arr[pivot_index]) cnt1 = 1;
+		i++;
+	}
+	i = arr_size - 1;
+	
+	while(i>pivot_index){
+		if(arr[i] >= arr[pivot_index]) cnt2 = 1;
+		i--;
+	}
+if(pivot_index == 0 &&cnt2 ==1) return 1;
+if(pivot_index == (arr_size - 1) && cnt1 == 1) return 1;
+if(cnt1 == 1&&cnt2 == 1)return 1;
+return 0;	
+}
+
+int is_sorted(float arr[20], int size)
+{
+	int i;
+	for(i = 0;i < size;i++){
+		if(arr,size,arr[1]== 0) return 0;
+	}
+	return 1;
+}
